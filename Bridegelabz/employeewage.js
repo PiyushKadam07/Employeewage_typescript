7da98c254d4c1d16f6cc3577fe3b298ef2645625
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-08-06 10:41:54 +0530

    second commit

--- Bridegelabz/employeewage.js
+++ Bridegelabz/employeewage.js
@@ -10,3 +10,18 @@ if (emp_check == is_Absent){
 else{
     console.log("Employee is present");
 }
+
+var empwage = 0;
+const empcheck = Math.floor(Math.random() * 10) % 2;
+let emphr = 0;
+
+switch(empcheck){
+    case 0: emphr = 8;   //fulltime
+            break;
+    case 1: emphr = 4;   //partime
+            break;
+    default:
+            break;
+}
+empwage = emphr * 20; //time * wage per hr
+console.log("Employee wage is ",empwage);
