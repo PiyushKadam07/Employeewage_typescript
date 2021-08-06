a266ddfeb0caa85b3b7abb0ae626fc3b9de1aefd
Piyush Kadam <piyush.kadam@morningstar.com>
2021-08-06 10:18:29 +0530

    first commit

---
+++ Bridegelabz/employeewage.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Employee Wage computation");
+
+const is_Absent = 0;
+
+let emp_check = Math.floor(Math.random() * 10) % 2;
+
+if (emp_check == is_Absent){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present");
+}
