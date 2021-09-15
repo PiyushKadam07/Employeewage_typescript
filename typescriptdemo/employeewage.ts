9c7017561e6b005a582ff52ca2aa4b717927b456
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-09-15 12:46:57 +0530

    [Piyush] display welcome message & check user is absent or present

---
+++ typescriptdemo/employeewage.ts
@@ -0,0 +1,12 @@
+//welcome message
+console.log("Welcome to Employee Wage computation");
+
+//uc1 check user is present or not
+var emp_check = Math.floor(Math.random() * 10) % 2;
+
+if (emp_check == 0){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present");
+}
